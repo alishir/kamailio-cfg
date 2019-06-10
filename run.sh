dcbbfbca8d799f73b808e1e8a899f60867301f13
alishir <aj.shirvani@gmail.com>
2019-06-10 08:45:07 +0430

    do not daemonize creator.

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ SHM_MEMORY=8
 PKG_MEMORY=8
 USER=kamailio
 GROUP=kamailio
-sudo /usr/sbin/kamailio -dd -D -f $CFGFILE -m $SHM_MEMORY -M $PKG_MEMORY -u $USER -g $GROUP -Ee -S -T
+sudo /usr/sbin/kamailio -dd -DD -f $CFGFILE -m $SHM_MEMORY -M $PKG_MEMORY -u $USER -g $GROUP -Ee -S -T
 
